5e79e92674faba141bb073fd412d8ecb90c91dff
Spaarda <Jamsonkeil@ya.ru>
2019-11-16 16:43:01 +0300

    Свёрстано модальное окно. Внесены правки главной страницы

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@ $('.header__slider').slick({
 	dots: true,
 	dotsClass: 'hero__dots',
 	draggable: false,
-})
+});
+
+$('.top-menu__burger').on('click' , function() {
+	$('.modal').toggleClass('modal_active');
+});
